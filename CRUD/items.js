65ef4ebaaae45f704a499bb215306697f9a7938a
Joe Luna <le_luna@mail.fresnostate.edu>
2023-03-14 11:13:47 -0700

    cleaned up file placements to work properly

--- CRUD/items.js
+++ CRUD/items.js
@@ -1,7 +1,7 @@
 const express=require('express');
 const router=express.Router();
 const fs= require('fs');
-const data=JSON.parse(fs.readFileSync('data.json','utf-8'));
+const data=JSON.parse(fs.readFileSync('CRUD/data.json','utf-8'));
 
 
 router.get('/', function(req,res){
