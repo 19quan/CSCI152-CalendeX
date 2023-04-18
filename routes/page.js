900328b1f594420a0424bce80f6cdb0060f11f57
Joe Luna <le_luna@mail.fresnostate.edu>
2023-04-17 18:17:56 -0700

    updates json with database
    Takes the json template for events and adds and updates it with the information from the database.

--- routes/page.js
+++ routes/page.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const Model = require('../models/pageModel');
+const events = require('../models/eventModel');
+const fs= require('fs');
+const data2=JSON.parse(fs.readFileSync('template.json','utf-8'));
+const eventdata=JSON.parse(fs.readFileSync('eventtemplate.json','utf-8'));
+
 
 const router = express.Router()
 
@@ -7,8 +12,18 @@ module.exports = router;
 
 router.get('/get', async (req, res) => {
     try{
-        const data = await Model.find();
-        res.json(data)
+
+        const data = data2.content[1].content[0].content[2].items;
+        const event = await events.find();
+        for(let i=0;i<Object.keys(event).length;i++)
+        {
+            data[i].title=event[i].title;
+            data[i].description=event[i].desc;
+            data[i].datetimeSecondaryLine=event[i].startDate;
+            data[i+1]=eventdata;
+        }
+        data2.content[1].content[0].content[2].items=data;
+        res.json(data2)
     }
     catch(error){
         res.status(500).json({message: error.message})
